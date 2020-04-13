d4b3f846e302cc899837b4d18c86323c6b59d275
ashuashenafi <ashuashenafi@gmail.com>
2020-04-13 15:00:52 -0400

    second updated

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -5,7 +5,7 @@ This is the first java program which will print out a message. Hello World.
 public class MyFirstJava {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello Ashenafi!");
 
     }
 }
