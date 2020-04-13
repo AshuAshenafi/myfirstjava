8687154552bbbbc1258cb5f73b05c645ea2d4504
ashuashenafi <ashuashenafi@gmail.com>
2020-04-13 14:08:59 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,11 @@
+
+/*
+This is the first java program which will print out a message. Hello World.
+ */
+public class MyFirstJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
