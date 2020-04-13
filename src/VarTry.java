ac749730ce40c44224a21ba11e321cb8a66df0aa
ashuashenafi <ashuashenafi@gmail.com>
2020-04-13 16:26:21 -0400

    first commit

---
+++ src/VarTry.java
@@ -0,0 +1,11 @@
+public class VarTry {
+    public static void main(String[] args) {
+        int testscore = 50;                     // declares and assigns test score
+        double price = 24.99;                   // declares and assigns price value
+        String username = "Bobby Johnson";      // declares and assigns username
+
+        System.out.println("Test Score initialized is: " + testscore);
+        System.out.println("Price tag: " + price);
+        System.out.println("Username: " + username);
+    }
+}
